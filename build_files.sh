c8d610097b5d5ffc73181291212e04b843693216
dhruvkej9 <dhruvkejri9@gmail.com>
2023-03-06 16:37:26 +0530

    package

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,7 @@
 echo " BUILD START"
+sudo pip install virtualenv
+virtualenv newenv
+source newenv/bin/activate
 python -m pip install -r requirements.txt
 python manage.py collectstatic --noinput --clear
 echo " BUILD END" 
