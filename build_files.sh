9c1f1f62f23492da5e063f8888f83b8c583f84c4
dhruvkej9 <dhruvkejri9@gmail.com>
2023-03-06 16:41:40 +0530

    NEweswsdw

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo " BUILD START"
-python -m pip install --ignore-installed pip==21.2.4 --disable-pip-version-check --no-warn-script-location
+python -m ensurepip --upgrade
 python -m pip install -r requirements.txt
 python manage.py collectstatic --noinput --clear
 echo " BUILD END" 
