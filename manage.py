6b78d46258bfca23469267703867a25cb8f5a9f7
dhruvkej9 <dhruvkejri9@gmail.com>
2023-03-06 16:00:05 +0530

    package

--- manage.py
+++ manage.py
@@ -9,12 +9,8 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Aim_Trainer.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+    except:
+        raise ImportError("Can not import django")
     execute_from_command_line(sys.argv)
 
 
