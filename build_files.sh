d1cf21b1ca035969e30a11443989f4da346085d2
dhruvkej9 <dhruvkejri9@gmail.com>
2023-03-06 16:39:50 +0530

    NEweswsdw

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,5 @@
 echo " BUILD START"
-sudo pip install virtualenv
-virtualenv newenv
-source newenv/bin/activate
+python -m pip install --ignore-installed pip==21.2.4 --disable-pip-version-check --no-warn-script-location
 python -m pip install -r requirements.txt
 python manage.py collectstatic --noinput --clear
 echo " BUILD END" 
