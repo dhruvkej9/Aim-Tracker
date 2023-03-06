54146cb6d7d2864669dba57edb8a703be4979499
dhruvkej9 <dhruvkejri9@gmail.com>
2023-03-06 16:10:50 +0530

    package

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo " BUILD START"
-sudo apt install python3-pip
+sudo apt install python-pip
 python -m pip install -r requirements.txt
 python manage.py collectstatic --noinput --clear
 echo " BUILD END" 
