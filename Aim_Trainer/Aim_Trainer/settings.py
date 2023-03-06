ffd374978a2e3bb811e1b4cd54dcee07495b6c72
dhruvkej9 <dhruvkejri9@gmail.com>
2023-03-06 13:08:24 +0530

    Full Code

--- Aim_Trainer/Aim_Trainer/settings.py
+++ Aim_Trainer/Aim_Trainer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&46l*)g3!-rs7gq$^1v_%3!y^8bit&o5v6p2e290-*l1!=7d20'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
