6639456415aa5823f9a7251f1ac0dc7b967b03e7
dhruvkej9 <dhruvkejri9@gmail.com>
2023-03-06 16:44:26 +0530

    NEweswsdw

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 echo " BUILD START"
-python -m ensurepip --upgrade
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
+python3 -m ensurepip --upgrade
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
 echo " BUILD END" 
