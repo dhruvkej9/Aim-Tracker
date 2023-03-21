04267a34606b39322cf937d9d14e3b4c4d869034
dhruvkej9 <dhruvkejri9@gmail.com>
2023-03-21 21:07:24 +0530

    Changes

--- Aim_Trainer/settings.py
+++ Aim_Trainer/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "get_random_secret_key()"
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # SECURE_SSL_REDIRECT = True
 
