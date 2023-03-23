0f4769eda2fb2824f7af19fa83c311285fdc38be
dhruvkej9 <dhruvkejri9@gmail.com>
2023-03-23 23:22:21 +0530

    FIxed

--- Aim_Trainer/urls.py
+++ Aim_Trainer/urls.py
@@ -18,19 +18,16 @@ from django.urls import include, path, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.static import serve
-
 # static_urlpatterns = [
     
 # ]
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('Aim_trainer_App.urls')),
+    path('', include('Aim_trainer_App.urls')),   
 ]
-
-from django.conf import settings
-from django.conf.urls.static import static
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
- urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
- urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ urlpatterns += path('admin/', admin.site.urls)
+ 
