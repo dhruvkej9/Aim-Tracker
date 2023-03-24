09c6be31faf549bf084028706903856779d7c9a2
dhruvkej9 <dhruvkejri9@gmail.com>
2023-03-24 17:24:22 +0530

    Images fix

--- Aim_Trainer/urls.py
+++ Aim_Trainer/urls.py
@@ -29,5 +29,5 @@ urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
- urlpatterns += path('admin/', admin.site.urls)
+ urlpatterns.append(path('admin/', admin.site.urls))
  
