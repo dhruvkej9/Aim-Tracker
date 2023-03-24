5c77219cd2fb18d54fa1ad5c20b003f015a5fb97
dhruvkej9 <dhruvkejri9@gmail.com>
2023-03-24 19:15:23 +0530

    Fixed Vercel issue 1

--- Aim_Trainer/settings.py
+++ Aim_Trainer/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # SECURE_SSL_REDIRECT = True
 
@@ -40,7 +40,7 @@ CSRF_COOKIE_SECURE = True
 
 # SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
-ALLOWED_HOSTS = ['127.0.0.1','dhruvkej9.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','dhruvkej9.pythonanywhere.com','.vercel.app', '.now.sh']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
